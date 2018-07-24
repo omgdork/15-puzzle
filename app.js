7fb042bf42d2dc026709426028dcbad88a18e1ae
Dork <omgdork@yahoo.com>
2018-07-24 16:34:08 +0800

    Changed app.js into a class.

--- app.js
+++ app.js
@@ -3,24 +3,37 @@
 import Utilities from './utilities/utilities.js';
 import Board from './components/board.js';
 
+class App {
+  constructor(container, txtColumns, txtRows, txtTileSize, btnAdd) {
+    this.container = container;
+    this.txtColumns = txtColumns;
+    this.txtRows = txtRows;
+    this.txtTileSize = txtTileSize;
+    this.btnAdd = btnAdd;
+  }
+
+  init() {
+    this.btnAdd.addEventListener('click', () => {
+      const columns = parseInt(this.txtColumns.value || 4, 10);
+      const rows = parseInt(this.txtRows.value || 4, 10);
+      const tileSize = parseInt(this.txtTileSize.value || 100, 10);
+      const newBoard = new Board(columns, rows, tileSize);
+
+      this.container.appendChild(newBoard.element);
+      newBoard.element.focus();
+    });
+  }
+}
+
 (() => {
-  function init() {
+  Utilities.ready(() => {
     const container = document.querySelector('.app');
     const txtColumns = document.getElementById('txt-columns');
     const txtRows = document.getElementById('txt-rows');
     const txtTileSize = document.getElementById('txt-tile-size');
     const btnAdd = document.getElementById('btn-add');
+    const app = new App(container, txtColumns, txtRows, txtTileSize, btnAdd);
 
-    btnAdd.addEventListener('click', () => {
-      const columns = parseInt(txtColumns.value || 4, 10);
-      const rows = parseInt(txtRows.value || 4, 10);
-      const tileSize = parseInt(txtTileSize.value || 100, 10);
-      const newBoard = new Board(columns, rows, tileSize);
-
-      container.appendChild(newBoard.element);
-      newBoard.element.focus();
-    });
-  }
-
-  Utilities.ready(init);
+    app.init();
+  });
 })();
